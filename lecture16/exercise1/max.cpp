95f5edd62c2292b1c9e41864deb02ab508df16ab
DavidBalash <david.balash@richmond.edu>
2023-10-26 11:23:35 -0400

    Upate

--- lecture16/exercise1/max.cpp
+++ lecture16/exercise1/max.cpp
@@ -1,26 +1,19 @@
 #include <iostream>
 using namespace std;
 
-
-int max(int x, int y)
+int maxValue(int x, int y)
 {
     if (x > y)
-    {
         return x;
-    }
     else
-    {
         return y;
-    }
 }
 
 int main()
 {
     int x = 10;
     int y = 6;
-
-    cout << "The max of x and y is " << max(x, y) << endl;
-
+    cout << "The max value of x and y is " << maxValue(x, y) << endl;
     return 0;
 }
 
