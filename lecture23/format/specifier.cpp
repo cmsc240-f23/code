e86575b0c93794de7d6d26449b1d938c2b366745
DavidBalash <david.balash@richmond.edu>
2023-11-21 09:34:45 -0500

    update

--- lecture23/format/specifier.cpp
+++ lecture23/format/specifier.cpp
@@ -1,8 +1,14 @@
 #include <cstdio>
+#include <iostream>
+using namespace std;
 
 int main()
 {
+    // C style 
     printf("Number is %d, floating point is %f, hex is %x \n", 123, 3.141592, 255);
+
+    // C++ style
+    cout << "Number is " << 123 << ", floating point is " << 3.141592 << ", hex is " << hex << 255 << endl;
 
     return 0;
 }
