2b6bad9e0218fbcad679f5a642a91c5485933f39
DavidBalash <david.balash@richmond.edu>
2023-11-21 02:49:44 -0500

    Update

---
+++ lecture23/format/stringVulnFix.cpp
@@ -0,0 +1,22 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+#define BUFFER_SIZE 16384
+
+int main() 
+{
+    // Create a buffer for user input.
+    char* buffer = new char[BUFFER_SIZE];
+ 
+    // Get data from a user. 
+    cout << "Enter a string: " << endl;
+    cin.getline(buffer, BUFFER_SIZE);
+
+    // Print with format specifier.
+    printf("%s", buffer);
+
+    cout << endl;
+
+    return 0;
+}
